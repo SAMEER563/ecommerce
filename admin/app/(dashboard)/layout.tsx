39de0b2e8ef30e0011a9a43affcfe2f679cc0b9e
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-04-16 23:40:04 +0530

    name change

--- admin/app/(dashboard)/layout.tsx
+++ admin/app/(dashboard)/layout.tsx
@@ -10,8 +10,8 @@ import { ToasterProvider } from "@/lib/ToasterProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Borcelle - Admin Dashboard",
-  description: "Admin dashboard to manage Borcelle's data",
+  title: " Admin Dashboard",
+  description: "Admin dashboard to manage Ecommercy data",
 };
 
 export default function RootLayout({
